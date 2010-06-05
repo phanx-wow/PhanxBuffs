32d1c544a56d1a3c52f3e7477d7b711999e649ba
Phanx <addons@phanx.net>
2010-06-05 15:35:26 +0000

    - Fix time remaining in temp enchant source tooltips - Add example buff to ignore list so noobs know how to add their own - Add copyright and license information - Add reamde

--- Debuffs.lua
+++ Debuffs.lua
@@ -1,5 +1,11 @@
 --[[--------------------------------------------------------------------
-	PhanxBuffs - Debuffs
+	PhanxBuffs
+	Replaces default player buff, debuff, and temporary enchant frames.
+	by Phanx < addons@phanx.net >
+	http://www.wowinterface.com/downloads/info-PhanxBuffs.html
+	http://wow.curse.com/downloads/wow-addons/details/phanxbuffs.aspx
+	Copyright © 2010 Alyssa "Phanx" Kinley
+	See README for license terms and other information.
 ----------------------------------------------------------------------]]
 
 PhanxDebuffFrame = CreateFrame("Frame")
