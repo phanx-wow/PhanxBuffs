4696453690ccbdb397e03e8cb30385b244c191b8
Phanx <addons@phanx.net>
2010-06-15 05:34:38 +0000

    - Check whether the player is in a vehicle when zoning, since no event is fired when the player leaves a vehicle due to zoning - Add support for ButtonFacade (needs testing) - Add empty localization file

---
+++ Localization.lua
@@ -0,0 +1,70 @@
+--[[--------------------------------------------------------------------
+	PhanxBuffs
+	Replaces default player buff, debuff, and temporary enchant frames.
+	by Phanx < addons@phanx.net >
+	http://www.wowinterface.com/downloads/info16874-PhanxBuffs.html
+	http://wow.curse.com/downloads/wow-addons/details/phanxbuffs.aspx
+	Copyright © 2010 Phanx. See README for license terms.
+----------------------------------------------------------------------]]
+
+local locale = GetLocale()
+if locale:match("^en") then return end
+
+local _, ns = ...
+local L = ns.L
+
+------------------------------------------------------------------------
+--	German / Deutsch
+--	Last updated YYYY-MM-DD by YourName
+------------------------------------------------------------------------
+
+if locale == "deDE" then
+return end
+
+------------------------------------------------------------------------
+--	Spanish and Latin American Spanish / Español (EU) y Español (AL)
+--	Last updated YYYY-MM-DD by YourName
+------------------------------------------------------------------------
+
+if locale == "esES" or locale == "esMX" then
+return end
+
+------------------------------------------------------------------------
+--	French / Français
+--	Last updated YYYY-MM-DD by YourName
+------------------------------------------------------------------------
+
+if locale == "frFR" then
+return end
+
+------------------------------------------------------------------------
+--	Russian / Русский
+--	Last updated YYYY-MM-DD by YourName
+------------------------------------------------------------------------
+
+if locale == "ruRU" then
+return end
+
+------------------------------------------------------------------------
+--	Korean / 한국어
+--	Last updated YYYY-MM-DD by YourName
+------------------------------------------------------------------------
+
+if locale == "koKR" then
+return end
+
+------------------------------------------------------------------------
+--	Simplified Chinese / 简体中文
+--	Last updated YYYY-MM-DD by YourName
+------------------------------------------------------------------------
+
+if locale == "zhCN" then
+return end
+
+------------------------------------------------------------------------
+--	Traditional Chinese / 正體中文
+--	Last updated YYYY-MM-DD by YourName
+------------------------------------------------------------------------
+
+if locale == "zhTW" then
+return end
