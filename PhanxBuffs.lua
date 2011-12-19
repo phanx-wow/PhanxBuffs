0509a8dbf2191fc86da2fcd99a5cb96761a97964
Phanx <addons@phanx.net>
2011-12-19 07:52:04 +0000

    - Update skinning support from ButtonFacade to Masque (still needs work re: resizing icons) - Update Curse download URL

--- PhanxBuffs.lua
+++ PhanxBuffs.lua
@@ -4,7 +4,7 @@
 	Written by Phanx <addons@phanx.net>
 	Copyright © 2010–2011 Phanx. Some rights reserved. See LICENSE.txt for details.
 	http://www.wowinterface.com/downloads/info16874-PhanxBuffs.html
-	http://wow.curse.com/downloads/wow-addons/details/phanxbuffs.aspx
+	http://www.curse.com/addons/wow/phanxbuffs
 ----------------------------------------------------------------------]]
 
 local db
