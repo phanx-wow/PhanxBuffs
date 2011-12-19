2673156d12df15aef439b1da90b963881883f53b
Phanx-28751 <Phanx-28751@6433d1c3-37e1-440b-bee2-e1399f6a98f9>
2011-12-19 07:52:04 +0000

    - Updated skinning support from ButtonFacade to Masque (still needs work re: resizing icons) - Updated Curse download URL

--- trunk/Localization/Locale-koKR.lua
+++ trunk/Localization/Locale-koKR.lua
@@ -4,7 +4,7 @@
 	Written by Phanx <addons@phanx.net>
 	Copyright © 2010–2011 Phanx. Some rights reserved. See LICENSE.txt for details.
 	http://www.wowinterface.com/downloads/info16874-PhanxBuffs.html
-	http://wow.curse.com/downloads/wow-addons/details/phanxbuffs.aspx
+	http://www.curse.com/addons/wow/phanxbuffs
 ------------------------------------------------------------------------
 	Korean Localization (한국어)
 	Last updated 2010-09-07 by Bruteforce
