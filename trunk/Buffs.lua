4c8b11959b6d99d2ccf18f79035637ed5cdff47b
Phanx-28751 <Phanx-28751@6433d1c3-37e1-440b-bee2-e1399f6a98f9>
2012-09-09 13:37:12 +0000

    - Reverted text placement changes - Attempt to compensate for the outline width by moving the text slightly - Update the README

--- trunk/Buffs.lua
+++ trunk/Buffs.lua
@@ -120,7 +120,7 @@ local buttons = setmetatable({ }, { __index = function(t, i)
     button.count:SetShadowOffset(1, -1)
 
 	button.timer = button:CreateFontString(nil, "OVERLAY")
-	button.timer:SetPoint("CENTER", button, "BOTTOM")
+	button.timer:SetPoint("TOP", button, "BOTTOM")
     button.timer:SetShadowOffset(1, -1)
 
 	if PhanxBorder then
@@ -168,6 +168,17 @@ function PhanxBuffFrame:UpdateLayout()
 
 		button.count:SetFont(fontFace, 18 * fontScale, fontOutline)
 		button.timer:SetFont(fontFace, 14 * fontScale, fontOutline)
+
+		if fontOutline == "THICKOUTLINE" then
+			button.count:SetPoint("CENTER", button, "TOP", 2, -1)
+			button.timer:SetPoint("TOP", button, "BOTTOM", 2, -1)
+		elseif fontOutline == "OUTLINE" then
+			button.count:SetPoint("CENTER", button, "TOP", 1, 0)
+			button.timer:SetPoint("TOP", button, "BOTTOM", 1, 0)
+		else
+			button.count:SetPoint("CENTER", button, "TOP", 0, 0)
+			button.timer:SetPoint("TOP", button, "BOTTOM", 0, 0)
+		end
 	end
 
 	self:ClearAllPoints()
