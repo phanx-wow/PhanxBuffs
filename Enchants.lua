6155776751bfe6ecdcbe32d33f901dfbdce0ca32
Phanx <addons@phanx.net>
2012-09-16 12:51:20 +0000

    - Fix vehicle switching for WoW 5.0

--- Enchants.lua
+++ Enchants.lua
@@ -388,13 +388,11 @@ function PhanxTempEnchantFrame:SPELLS_CHANGED()
 end
 
 function PhanxTempEnchantFrame:UNIT_INVENTORY_CHANGED(unit)
-	if unit == "player" then
-		dirty = true
-	end
+	dirty = true
 end
 
 function PhanxTempEnchantFrame:UNIT_ENTERED_VEHICLE(unit)
-	if unit == "player" and SecureCmdOptionParse( "[bonusbar:5]" ) then
+	if UnitHasVehicleUI(unit) then
 		inVehicle = true
 		self:Hide()
 		PhanxBuffFrame.buttons[1]:SetPoint("TOP" .. db.growthAnchor, PhanxBuffFrame)
@@ -402,15 +400,13 @@ function PhanxTempEnchantFrame:UNIT_ENTERED_VEHICLE(unit)
 end
 
 function PhanxTempEnchantFrame:UNIT_EXITED_VEHICLE(unit)
-	if unit == "player" then
-		inVehicle = nil
-		dirty = true
-		self:Show()
-	end
+	inVehicle = nil
+	dirty = true
+	self:Show()
 end
 
 function PhanxTempEnchantFrame:PLAYER_ENTERING_WORLD()
-	local inVehicleNow = UnitInVehicle("player")
+	local inVehicleNow = UnitHasVehicleUI("player")
 	if inVehicle and not inVehicleNow then
 		return self:UNIT_EXITED_VEHICLE("player")
 	elseif inVehicleNow and not inVehicle then
@@ -489,7 +485,7 @@ function PhanxTempEnchantFrame:Load()
 	self:RegisterEvent("BAG_UPDATE")
 	self:RegisterEvent("PLAYER_ENTERING_WORLD")
 	self:RegisterEvent("SPELLS_CHANGED")
-	self:RegisterEvent("UNIT_ENTERED_VEHICLE")
-	self:RegisterEvent("UNIT_EXITED_VEHICLE")
-	self:RegisterEvent("UNIT_INVENTORY_CHANGED")
+	self:RegisterUnitEvent("UNIT_ENTERED_VEHICLE", "player")
+	self:RegisterUnitEvent("UNIT_EXITED_VEHICLE", "player")
+	self:RegisterUnitEvent("UNIT_INVENTORY_CHANGED", "player")
 end
