6c6176d4dfa8a9deea30dc8fc8de0c2a239f3f15
Phanx <addons@phanx.net>
2015-03-02 05:33:23 -0800

    TOC 60100

--- PhanxBuffs.lua
+++ PhanxBuffs.lua
@@ -1,7 +1,7 @@
 --[[--------------------------------------------------------------------
 	PhanxBuffs
 	Replacement player buff, debuff, and temporary enchant frames.
-	Copyright (c) 2010-2014 Phanx <addons@phanx.net>. All rights reserved.
+	Copyright (c) 2010-2015 Phanx <addons@phanx.net>. All rights reserved.
 	http://www.wowinterface.com/downloads/info16874-PhanxBuffs.html
 	http://www.curse.com/addons/wow/phanxbuffs
 	https://github.com/Phanx/PhanxBuffs
